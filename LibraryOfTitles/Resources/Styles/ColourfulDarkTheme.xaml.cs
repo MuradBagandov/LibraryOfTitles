bd30eb22315e32be0602f8516616cb78c3561a70
Гаджимурад Багандов <muradb20398@gmail.com>
2021-05-21 18:57:10 +0300

    Добавление темной темы

---
+++ LibraryOfTitles/Resources/Styles/ColourfulDarkTheme.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LibraryOfTitles.Resources.Styles
+{
+    /// <summary>
+    /// Логика взаимодействия для ColourfulDarkTheme.xaml
+    /// </summary>
+    public partial class ColourfulDarkTheme
+    {
+        private void CloseWindow_Event(object sender, RoutedEventArgs e)
+        {
+            if (e.Source != null)
+                try { CloseWind(Window.GetWindow((FrameworkElement)e.Source)); } catch { }
+        }
+        private void AutoMinimize_Event(object sender, RoutedEventArgs e)
+        {
+            if (e.Source != null)
+                try { MaximizeRestore(Window.GetWindow((FrameworkElement)e.Source)); } catch { }
+        }
+        private void Minimize_Event(object sender, RoutedEventArgs e)
+        {
+            if (e.Source != null)
+                try { MinimizeWind(Window.GetWindow((FrameworkElement)e.Source)); } catch { }
+        }
+
+        public void CloseWind(Window window) => window.Close();
+        public void MaximizeRestore(Window window)
+        {
+            if (window.WindowState == WindowState.Maximized)
+                window.WindowState = WindowState.Normal;
+            else if (window.WindowState == WindowState.Normal)
+                window.WindowState = WindowState.Maximized;
+        }
+        public void MinimizeWind(Window window) => window.WindowState = WindowState.Minimized;
+    }
+}
